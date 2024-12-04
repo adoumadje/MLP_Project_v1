b895143b38ea8db8f99ca7eaca8ebe931cd293fa
adoumadje <adoumadjecaleb@gmail.com>
2024-12-05 00:05:19 +0100

    addition and substraction

--- substraction.asm
+++ substraction.asm
@@ -5,28 +5,24 @@ STDIN equ 0
 STDOUT equ 1
 
 section .data
-
     msg1 db "num1 = "
     len1 equ $ - msg1
 
     msg2 db "num2 = "
     len2 equ $ - msg2
 
-    msg3 db "num1 + num2 = "
+    msg3 db "num1 - num2 = "
     len3 equ $ - msg3
 
 section .bss
-
     num1 resb 2
     num2 resb 2
     res resb 1
 
 section .text
-
     global _start       ;must be declared for using gcc
 
-_start:
-
+_start:                 ;tell linker entry point
     mov eax, SYS_WRITE
     mov ebx, STDOUT
     mov ecx, msg1
@@ -63,7 +59,7 @@ _start:
     mov ebx, [num2]
     sub ebx, '0'
 
-    add eax, ebx
+    sub eax, ebx
     add eax, '0'
 
     mov [res], eax
@@ -79,5 +75,3 @@ exit:
     mov eax, SYS_EXIT
     xor ebx, ebx
     int 0x80
-
-
